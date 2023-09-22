c2aa9b3ff0ac45dc8537b299093fc48ffc0dbf45
DIOURI Yassine <yassinediouri92@gmail.com>
2023-09-22 09:17:23 +0200

    design the first header menu links

--- src/app/common/header-menu/header-menu.component.ts
+++ src/app/common/header-menu/header-menu.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
-  styleUrls: ['./header-menu.component.scss']
+  styleUrls: ['./header-menu.component.scss'],
 })
-export class HeaderMenuComponent {
-
-}
+export class HeaderMenuComponent {}
